4e0829e09e595ac84bb6e3d7e9f970f76887eb88
Lude Bakema <lude.bakema@philips.com>
2021-06-23 17:26:35 +0200

    z

--- batch_files/UnitTest.bat
+++ batch_files/UnitTest.bat
@@ -1,2 +1,3 @@
 "C:\Program Files (x86)\National Instruments\Shared\LabVIEW CLI\LabVIEWCLI.exe" -LogToConsole true -OperationName RunVI -VIPath "C:\Data\projects\jenkins\repo\Jenkins_test\jenkins_unit_test.vi"
+pause
 
