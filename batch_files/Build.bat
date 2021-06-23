bbcf98ac9a4e5f2470cde342b2d343589c75c585
Lude Bakema <lude.bakema@philips.com>
2021-06-23 16:41:06 +0200

    updated batch files

--- batch_files/Build.bat
+++ batch_files/Build.bat
@@ -1,4 +1,4 @@
 cd..
 SET cur_dir=%cd%
 @echo %cur_dir%
-LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "%cur_dir%\jenkins_test.lvproj" -BuildSpecName "App"
+LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "C:\Data\projects\jenkins\repo\Jenkins_test\batch_files\jenkins_test.lvproj" -BuildSpecName "App"
--- batch_files/UnitTest.bat
+++ batch_files/UnitTest.bat
@@ -1,4 +1,4 @@
 cd..
 SET cur_dir=%cd%
 @echo %cur_dir%
-LabVIEWCLI -LogToConsole true -OperationName RunVI -VIPath "%cur_dir%\jenkins_unit_test.vi"
+LabVIEWCLI -LogToConsole true -OperationName RunVI -VIPath "C:\Data\projects\jenkins\repo\Jenkins_test\batch_files\jenkins_unit_test.vi"
