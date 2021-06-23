97ed90e9dd6de022195e15fd46616c05c359b06b
Lude Bakema <lude.bakema@philips.com>
2021-06-23 16:10:06 +0200

    created batch files

---
+++ batch_files/Build.bat
@@ -0,0 +1,4 @@
+cd..
+SET cur_dir=%cd%
+@echo %cur_dir%
+LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "%cur_dir%\jenkins_test.lvproj" -BuildSpecName "App"
---
+++ batch_files/UnitTest.bat
@@ -0,0 +1,4 @@
+cd..
+SET cur_dir=%cd%
+@echo %cur_dir%
+LabVIEWCLI -LogToConsole true -OperationName RunVI -VIPath "%cur_dir%\jenkins_unit_test.vi"
