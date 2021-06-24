e943086198ea56e04221d5202e6edaa113e1fc50
Lude Bakema <lude.bakema@philips.com>
2021-06-24 11:19:08 +0200

    batch file unit test updated

--- batch_files/UnitTest.bat
+++ batch_files/UnitTest.bat
@@ -1,3 +1,4 @@
-"C:\Program Files (x86)\National Instruments\Shared\LabVIEW CLI\LabVIEWCLI.exe" -LogToConsole true -OperationName RunVI -VIPath "C:\Data\projects\jenkins\repo\Jenkins_test\jenkins_unit_test.vi"
-pause
+SET cur_dir=%cd%
+echo %cur_dir%
+LabVIEWCLI -LogToConsole true -OperationName RunVI -VIPath "%cur_dir%\Jenkins_test\jenkins_unit_test.vi"
 
