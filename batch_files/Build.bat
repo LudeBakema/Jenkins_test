7f2036674cf027bdd7034ccc834faa4daa86b5c0
Lude Bakema <lude.bakema@philips.com>
2021-06-29 12:56:46 +0200

    build.bat update

--- batch_files/Build.bat
+++ batch_files/Build.bat
@@ -1,4 +1,4 @@
 SET cur_dir=%cd%
 echo %cur_dir%
-LabVIEW CLI\LabVIEWCLI.exe -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "%cur_dir%\jenkins_test.lvproj" -TargetName "My Computer" -BuildSpecName "App"
+LabVIEW CLI -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "%cur_dir%\jenkins_test.lvproj" -TargetName "My Computer" -BuildSpecName "App"
 
